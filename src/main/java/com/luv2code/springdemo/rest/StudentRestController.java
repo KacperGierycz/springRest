f058200022d00ed276ed51cef34691cc0b93ee43
Kacper <kacpergierycz@gmail.com>
2021-01-19 13:49:53 +0100

    cut excepion handlers from @RustController to @ControllerAdvice to handle all exceptions in one class and AOP global acces to all aces with @CA!

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -57,24 +57,8 @@ public class StudentRestController {
 		return theStudents.get(studentId);
 	}
 	
-	// Add an exception handler using @ExceptinoHandler
 	
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse>handleException(StudentNotFoundException exc){
-		
-		// Create a StudentErrorResponse
-		
-		StudentErrorResponse error= new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		// return ResponeEntity
-		
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-	}
-	
+
 }
 
 
